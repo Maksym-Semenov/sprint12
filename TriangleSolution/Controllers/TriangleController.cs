6d7cb9c7f953e8050a354ee3b8f328b3c2512388
Alice <alice@gmail.com>
2022-10-28 10:05:28 +0300

    first day

--- TriangleSolution/Controllers/TriangleController.cs
+++ TriangleSolution/Controllers/TriangleController.cs
@@ -1,62 +1,147 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Collections.Generic;
+using System.Text;
 using Triangles.Models;
 
 namespace Triangles.Controllers
 {
     public class TriangleController : Controller
     {
-        //Task_02
+        // Task_01 Create an Info action that shows the info about a triangle in a form as follows:
+        public string Info(Triangle tr)
+        {
+            if (IsValid(tr))
+            {
+                double[] nums = new double[3];
+                nums[0] = Convert.ToDouble(tr.Side1);
+                nums[1] = Convert.ToDouble(tr.Side2);
+                nums[2] = Convert.ToDouble(tr.Side3);
+                Array.Sort(nums);
+                //double temp;
+                //for (int i = 0; i < nums.Length - 1; i++)
+                //{
+                //    for (int j = i + 1; j < nums.Length; j++)
+                //    {
+                //        if (nums[i] > nums[j])
+                //        {
+                //            temp = nums[i];
+                //            nums[i] = nums[j];
+                //            nums[j] = temp;
+                //        }
+                //    }
+                //}
+                StringBuilder sb = new StringBuilder();
+                sb.Append("Triangle:");
+                sb.Append($"\n{nums[0]}, {nums[1]}, {nums[2]}");
+                sb.Append("\nReduced:");
+                sb.Append($"\n{nums[0] / Convert.ToDouble(Perimeter(tr))}, ");
+                sb.Append($"{nums[1] / Convert.ToDouble(Perimeter(tr))}, ");
+                sb.Append($"{nums[2] / Convert.ToDouble(Perimeter(tr))}");
+                sb.Append($"\n\nArea: {Area(tr)}");
+                sb.Append($"\nPerimeter: {Perimeter(tr)}");
+                return sb.ToString();
+                //Console.WriteLine($"Triangle:" +
+                //                    $"\n{nums[0]}, {nums[1]}, {nums[2]}" +
+                //                    $"\nReduced:" +
+                //                    $"\n{nums[0] / Convert.ToDouble(Perimeter(tr))}" +
+                //                    $", {nums[1] / Convert.ToDouble(Perimeter(tr))}" +
+                //                    $", {nums[2] / Convert.ToDouble(Perimeter(tr))}" +
+                //                    $"\nArea: {Area(tr)}" +
+                //                    $"\nPerimeter: {Perimeter(tr)}");
+                //Console.WriteLine("Triangle:");
+                //Console.WriteLine("Reduced:");
+                //Console.WriteLine($"{nums[0] / Convert.ToDouble(Perimeter(tr))}" +
+                //                  $",{nums[1] / Convert.ToDouble(Perimeter(tr))}" +
+                //                  $",{nums[2] / Convert.ToDouble(Perimeter(tr))}");
+                //Console.WriteLine($"Area: {Area(tr)}");
+                //Console.WriteLine($"Perimeter: {Perimeter(tr)}");
+            }
+            else
+                throw new Exception();
+            
+        }
+        // Task_02 Create an Area action that calculates the area of a triangle
         public string Area(Triangle tr)
         {
-            if (tr.Side1 > 0 && tr.Side2 > 0 && tr.Side3 > 0)
+            if (IsValid(tr))
             {
                 double halfPerimeter = (tr.Side1 + tr.Side2 + tr.Side3) / 2;
-                double result = (Math.Sqrt(halfPerimeter * (halfPerimeter - tr.Side1)
+                double area = (Math.Sqrt(halfPerimeter * (halfPerimeter - tr.Side1)
                                               * (halfPerimeter - tr.Side2)
                                               * (halfPerimeter - tr.Side3)));
-                return string.Format("{0:F4}", result);
+                return string.Format("{0:F4}", area);
             }
             else
                 throw new ArgumentException();
         }
-        //Task_05
-        public bool IsEquilateral(double side1, double side2, double side3)
+        // Task_03
+        public string Perimeter(Triangle tr)
         {
-            if ((side1 > 0 && side2 > 0 && side3 > 0))
+            if (IsValid(tr))
+                return (tr.Side1 + tr.Side2 + tr.Side3).ToString();
+            else
+                throw new ArgumentException();
+        }
+        // Task_05 Create an IsEquilateral action that returns boolean True or False
+        // depending on the triangle is equilateral or not
+        public bool IsEquilateral(Triangle tr)
+        {
+            if (IsValid(tr) && IsValid(tr.Side1, tr.Side2, tr.Side3))
             {
-                return (side1 == side2 || side1 == side3 || side2 == side3);
+                return (tr.Side1 == tr.Side2 || tr.Side1 == tr.Side3 || tr.Side2 == tr.Side3);
             }
             else
                 throw new ArgumentException();
         }
-        //Task_07
+        // Task_07 Create an AreCongruent action that returns boolean True or False
+        // depending on two triangles are congruent or not
         public bool AreCongruent(Triangle tr1, Triangle tr2)
         {
-            if (IsValid(tr1) && IsValid(tr2))
+            return (IsValid(tr1) && IsValid(tr2)
+                             && IsEqualDouble(tr1.Side1, tr2.Side1)
+                             && IsEqualDouble(tr1.Side2, tr2.Side2)
+                             && IsEqualDouble(tr1.Side3, tr2.Side3));
+        }
+        //Task_10
+        public string InfoGreatestArea(Triangle[] triangles)
+        {
+            double temp;
+            double maxArea = 0;
+            foreach (Triangle triangle in triangles)
             {
-                return (tr1.Side1 == tr2.Side1 && tr1.Side2 == tr2.Side2 && tr1.Side3 == tr2.Side3);
+                if (IsValid(triangle))
+                {
+                    temp = Convert.ToDouble(Area(triangle));
+                    if (temp > maxArea)
+                    {
+                        maxArea = temp;
+                    }
+                }
             }
-            else
-                throw new ArgumentException();
+            return maxArea;
         }
         private bool IsValid(double side1, double side2, double side3)
         {
             return (side1 + side2 >= side3
                  && side2 + side3 >= side1
                  && side1 + side3 >= side2
-                     && side1 > 0 
-                     && side2 > 0
-                     && side3 > 0);
+                 && side1 > 0 
+                 && side2 > 0
+                 && side3 > 0);
         }
         private bool IsValid(Triangle tr)
         {
             return (tr.Side1 + tr.Side2 >= tr.Side3
                  && tr.Side2 + tr.Side3 >= tr.Side1
                  && tr.Side1 + tr.Side3 >= tr.Side2
-                     && tr.Side1 > 0
-                     && tr.Side2 > 0
-                     && tr.Side3 > 0);
+                 && tr.Side1 > 0
+                 && tr.Side2 > 0
+                 && tr.Side3 > 0);
+        }
+        private bool IsEqualDouble(double db1, double db2)
+        {
+           return Math.Abs(db1 - db2) < 0.001;
         }
     }
 }
--- TriangleTests/TriangleTest.cs
+++ TriangleTests/TriangleTest.cs
@@ -32,50 +32,50 @@ namespace TriangleTests
             controller = new TriangleController();
         }
 
-        //[Theory]
-        //[InlineData(3, 4, 5, 12, 6)]
-        //[InlineData(5, 6, 7, 18, 14.70)]
-        //public void Info_WithCorrectOrderedSides_ReturnsStringWithData(double side1, double side2, double side3, double perimeter, double area)
-        //{
-        //    controller.Info(new Triangle(side1, side2, side3))
-        //        .Should().BeEquivalentTo(_generateOutput(side1, side2, side3, perimeter, area));
-        //}
+        [Theory]
+        [InlineData(3, 4, 5, 12, 6)]
+        [InlineData(5, 6, 7, 18, 14.70)]
+        public void Info_WithCorrectOrderedSides_ReturnsStringWithData(double side1, double side2, double side3, double perimeter, double area)
+        {
+            controller.Info(new Triangle(side1, side2, side3))
+                .Should().BeEquivalentTo(_generateOutput(side1, side2, side3, perimeter, area));
+        }
 
-        //[Theory]
-        //[InlineData(4, 3, 5, 12, 6)]
-        //[InlineData(6, 5, 7, 18, 14.70)]
-        //public void Info_WithCorrectUnOrdered1and2Sides_ReturnsStringWithData(double side1, double side2, double side3, double perimeter, double area)
-        //{
+        [Theory]
+        [InlineData(4, 3, 5, 12, 6)]
+        [InlineData(6, 5, 7, 18, 14.70)]
+        public void Info_WithCorrectUnOrdered1and2Sides_ReturnsStringWithData(double side1, double side2, double side3, double perimeter, double area)
+        {
 
-        //    controller.Info(new Triangle(side1, side2, side3))
-        //        .Should().BeEquivalentTo(_generateOutput(side2, side1, side3, perimeter, area));
-        //}
+            controller.Info(new Triangle(side1, side2, side3))
+                .Should().BeEquivalentTo(_generateOutput(side2, side1, side3, perimeter, area));
+        }
 
-        //[Theory]
-        //[InlineData(5, 4, 3, 12, 6)]
-        //[InlineData(7, 6, 5, 18, 14.70)]
-        //public void Info_WithCorrectReverseOrderedSides_ReturnsStringWithData(double side1, double side2, double side3, double perimeter, double area)
-        //{
+        [Theory]
+        [InlineData(5, 4, 3, 12, 6)]
+        [InlineData(7, 6, 5, 18, 14.70)]
+        public void Info_WithCorrectReverseOrderedSides_ReturnsStringWithData(double side1, double side2, double side3, double perimeter, double area)
+        {
 
-        //    controller.Info(new Triangle(side1, side2, side3))
-        //        .Should().BeEquivalentTo(_generateOutput(side3, side2, side1, perimeter, area));
-        //}
+            controller.Info(new Triangle(side1, side2, side3))
+                .Should().BeEquivalentTo(_generateOutput(side3, side2, side1, perimeter, area));
+        }
 
-        //[Theory]
-        //[InlineData(5, 4, 3, 6)]
-        //[InlineData(7, 6, 5, 14.69690)]
-        //public void Area_WithCorrectSides_ReturnsArea(double side1, double side2, double side3, double area)
-        //{
-        //    string.Format("{0:F4}", area).Should().BeEquivalentTo(controller.Area(new Triangle(side1, side2, side3))); ;
-        //}
+        [Theory]
+        [InlineData(5, 4, 3, 6)]
+        [InlineData(7, 6, 5, 14.69690)]
+        public void Area_WithCorrectSides_ReturnsArea(double side1, double side2, double side3, double area)
+        {
+            string.Format("{0:F4}", area).Should().BeEquivalentTo(controller.Area(new Triangle(side1, side2, side3))); ;
+        }
 
-        //[Theory]
-        //[InlineData(5, 4, 3, 12)]
-        //[InlineData(7, 6, 5, 18)]
-        //public void Perimeter_WitCorrectSides_ReturnsPerimeter(double side1, double side2, double side3, double perimeter)
-        //{
-        //    controller.Perimeter(new Triangle(side1, side2, side3)).ToString().Should().BeEquivalentTo(perimeter.ToString());
-        //}
+        [Theory]
+        [InlineData(5, 4, 3, 12)]
+        [InlineData(7, 6, 5, 18)]
+        public void Perimeter_WitCorrectSides_ReturnsPerimeter(double side1, double side2, double side3, double perimeter)
+        {
+            controller.Perimeter(new Triangle(side1, side2, side3)).ToString().Should().BeEquivalentTo(perimeter.ToString());
+        }
 
         //[Theory]
         //[InlineData(3, 4, 5, true)]
@@ -86,15 +86,15 @@ namespace TriangleTests
         //    controller.IsRightAngled(new Triangle(side1, side2, side3)).Should().Be(isRightAngled);
         //}
 
-        //[Theory]
-        //[InlineData(3, 3, 3, true)]
-        //[InlineData(6, 6, 6.00002, true)]
-        //[InlineData(6.00006, 6.00011, 6.00016, false)]
-        //[InlineData(6.00005, 6, 6.00004, true)]
-        //public void IsEquilateral_WithRCorrectSides_ReturnsWhetherEquilatera(double side1, double side2, double side3, bool isRightAngled)
-        //{
-        //    controller.IsEquilateral(new Triangle(side1, side2, side3)).Should().Be(isRightAngled);
-        //}
+        [Theory]
+        [InlineData(3, 3, 3, true)]
+        [InlineData(6, 6, 6.00002, true)]
+        [InlineData(6.00006, 6.00011, 6.00016, false)]
+        [InlineData(6.00005, 6, 6.00004, true)]
+        public void IsEquilateral_WithRCorrectSides_ReturnsWhetherEquilatera(double side1, double side2, double side3, bool isRightAngled)
+        {
+            controller.IsEquilateral(new Triangle(side1, side2, side3)).Should().Be(isRightAngled);
+        }
 
         //[Theory]
         //[InlineData(3, 3, 6, true)]
@@ -115,13 +115,13 @@ namespace TriangleTests
             controller.AreCongruent(new Triangle(side1, side2, side3), new Triangle(side4, side5, side6)).Should().BeTrue();
         }
 
-        //[Theory]
-        //[InlineData(3, 3, 6, 3, 4, 7)]
-        //[InlineData(3, 3, 6, 3, 3, 6.0003)]
-        //public void AreCongruent_WithNotEqualSides_ReturnsFalse(double side1, double side2, double side3, double side4, double side5, double side6)
-        //{
-        //    controller.AreCongruent(new Triangle(side1, side2, side3), new Triangle(side4, side5, side6)).Should().BeFalse();
-        //}
+        [Theory]
+        [InlineData(3, 3, 6, 3, 4, 7)]
+        [InlineData(3, 3, 6, 3, 3, 6.0003)]
+        public void AreCongruent_WithNotEqualSides_ReturnsFalse(double side1, double side2, double side3, double side4, double side5, double side6)
+        {
+            controller.AreCongruent(new Triangle(side1, side2, side3), new Triangle(side4, side5, side6)).Should().BeFalse();
+        }
 
         //[Theory]
         //[InlineData(3, 3, 6, 4, 4, 8)]
@@ -148,12 +148,12 @@ namespace TriangleTests
         //        .Should().BeEquivalentTo(_generateOutput(5, 6, 7, 18, 14.70));
         //}
 
-        //[Fact]
-        //public void InfoGreatestArea_CorrectTriangles_ReturnsStringWithData()
-        //{
-        //    controller.InfoGreatestArea(TrianglesArray)
-        //        .Should().BeEquivalentTo(_generateOutput(5.95, 6, 6, 17.95, 15.5));            
-        //}
+        [Fact]
+        public void InfoGreatestArea_CorrectTriangles_ReturnsStringWithData()
+        {
+            controller.InfoGreatestArea(TrianglesArray)
+                .Should().BeEquivalentTo(_generateOutput(5.95, 6, 6, 17.95, 15.5));
+        }
 
         public static Triangle[] TrianglesArray =
         new Triangle[]
